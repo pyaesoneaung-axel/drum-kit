361070c0c2e77837777337344f15c558288fec01
PYAE SONE AUNG <69912310+pyaesoneaung-axel@users.noreply.github.com>
2023-09-09 22:15:43 +0630

    Update index.js
    Optimize codes a bit

--- index.js
+++ index.js
@@ -1,37 +1,21 @@
-let drumSet = {
-  w: document.querySelector(".w"),
-  a: document.querySelector(".a"),
-  s: document.querySelector(".s"),
-  d: document.querySelector(".d"),
-  j: document.querySelector(".j"),
-  k: document.querySelector(".k"),
-  l: document.querySelector(".l"),
-};
-
+let drumSet = document.querySelectorAll(".drum");
+let keySet = ["w", "a", "s", "d", "j", "k", "l"];
 let drum = document.querySelector("body");
 
-// This doesn't work, don't know why yet
-// for (let oKey in drumSet) {
-//   drumSet[oKey].setAttribute("onClick", `playDrum(${oKey})`);
-// }
-
-drumSet.w.setAttribute("onClick", `playDrum("w")`);
-drumSet.a.setAttribute("onClick", `playDrum("a")`);
-drumSet.s.setAttribute("onClick", `playDrum("s")`);
-drumSet.d.setAttribute("onClick", `playDrum("d")`);
-drumSet.j.setAttribute("onClick", `playDrum("j")`);
-drumSet.k.setAttribute("onClick", `playDrum("k")`);
-drumSet.l.setAttribute("onClick", `playDrum("l")`);
+let i = 0;
+for (let drum of drumSet) {
+  // I have no clue why this is passing the "audio object with the id of ${keySet[i]}"
+  // Maybe cuz class-name and that id-name is the same?
+  drum.setAttribute("onClick", `playDrum(${keySet[i++]})`);
+}
 
 drum.addEventListener("keydown", function (e) {
   let key = e.key;
-  let keySet = ["w", "a", "s", "d", "j", "k", "l"];
   if (keySet.includes(key)) {
-    // console.log("h");
-    playDrum(key);
+    playDrum(document.getElementById(`${key}`));
   }
 });
 
 function playDrum(key) {
-  document.getElementById(`${key}`).play();
+  key.play();
 }
